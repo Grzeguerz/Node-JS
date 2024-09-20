d81c6da52361cce0a3b25391cae59e9fc2eadbc2
Grzeguerz <grzeguerz@gmail.com>
2024-09-20 03:32:32 -0700

    Added seccion 3: Reforzamiento de los temas JS

---
+++ 02-fundamentos/callbacks.js
@@ -0,0 +1,25 @@
+
+// setTimeout(() => {
+//     console.log( 'Hola Mundo ');
+// }, 1000);
+
+const getUsuarioByID = ( id, callback ) => {
+
+    const user = {
+        id,
+        nombre: 'Fernando',
+    }
+
+    setTimeout(() => {
+        callback( user );
+    }, 1500);
+
+}
+
+getUsuarioByID( 10, ( { id, nombre } ) => {
+    console.log( id );
+    console.log( nombre.toUpperCase() );
+});
+
+
+
---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,16 @@
+
+const nombre = 'Deadpool';
+const real = 'Wade Winston';
+
+const normal = nombre + ' is ' + real ;
+const template = `${ nombre } is ${ real }`;
+
+console.log( template );
+
+const html = `
+<h1>Hola</h1>
+<p>Mundo</p>
+`;
+
+console.log( html );
+
